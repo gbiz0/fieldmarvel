1f23bb0147b89f8b4633dceed89b043ea12363f9
gbiz0 <jardimbgustavo@gmail.com>
2023-07-11 19:03:44 -0300

    feat: update

--- src/app/characters/character/shared/character-api.service.ts
+++ src/app/characters/character/shared/character-api.service.ts
@@ -12,10 +12,9 @@ export class CharacterApiService {
   hash = ''
   urlAPI = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=d923f4cd4b6463a729c8eaa6820af452&hash=3850d1ad4bfbbe87d90900e4187ea65f'
   constructor(public http: HttpClient) {}
-
-
       getAllCharacters(): Observable<any> {
         return this.http.get<any>(this.urlAPI)
         .pipe(map((data: any) => data.data.results))
       }
+    
 }
